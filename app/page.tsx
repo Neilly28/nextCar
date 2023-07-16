9d9cdac8871c2a4360db3b78bc8d6ede505e0407
Neilly28 <neilpilarca@gmail.com>
2023-07-16 15:24:24 +0200

    add fetch api

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-import { CustomFilter, Hero, SearchBar } from "@/components";
-import Image from "next/image";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { fetchCars } from "@/utils";
 
-export default function Home() {
+export default async function Home() {
+  // fetch api
+  const allCars = await fetchCars();
+  console.log(allCars);
+
+  // check if there is no data from api
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -17,6 +24,20 @@ export default function Home() {
             <CustomFilter title="year" />
           </div>
         </div>
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops no results!</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
